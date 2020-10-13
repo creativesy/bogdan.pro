7dca5c3b2395db44dae338382183cdc2c97199a1
creativesy <72804430+creativesy@users.noreply.github.com>
2020-10-13 16:35:47 +0300

    Changes

--- wp-login.php
+++ wp-login.php
@@ -4,11 +4,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Autentificare &lsaquo; bogdan.pro &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/https://creativesy.github.io/bogdan.pro/wp-includes/css/dashicons.min.css?ver=5.5.1' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/https://creativesy.github.io/bogdan.pro/wp-includes/css/buttons.min.css?ver=5.5.1' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/https://creativesy.github.io/bogdan.pro/wp-admin/css/forms.min.css?ver=5.5.1' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/https://creativesy.github.io/bogdan.pro/wp-admin/css/l10n.min.css?ver=5.5.1' media='all' />
-<link rel='stylesheet' id='login-css'  href='/https://creativesy.github.io/bogdan.pro/wp-admin/css/login.min.css?ver=5.5.1' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-includes/css/dashicons.min.css?ver=5.5.1' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-includes/css/buttons.min.css?ver=5.5.1' media='all' />
+<link rel='stylesheet' id='forms-css'  href='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-admin/css/forms.min.css?ver=5.5.1' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-admin/css/l10n.min.css?ver=5.5.1' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-admin/css/login.min.css?ver=5.5.1' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -20,7 +20,7 @@
 		<div id="login">
 		<h1><a href="https://ro.wordpress.org/">Propulsat de WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="/https://creativesy.github.io/bogdan.pro/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="http://localhost/https:/creativesy.github.io/bogdan.pro/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Nume utilizator sau adresă email</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
@@ -44,25 +44,25 @@
 		</form>
 
 					<p id="nav">
-								<a href="/https://creativesy.github.io/bogdan.pro/wp-login.php?action=lostpassword">Ai uitat parola?</a>
+								<a href="http://localhost/https:/creativesy.github.io/bogdan.pro/wp-login.php?action=lostpassword">Ai uitat parola?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="/https://creativesy.github.io/bogdan.pro/">
+				<p id="backtoblog"><a href="http://localhost/https:/creativesy.github.io/bogdan.pro/">
 		&larr; Înapoi la bogdan.pro		</a></p>
 			</div>
-	<script src='/https://creativesy.github.io/bogdan.pro/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp' id='jquery-core-js'></script>
+	<script src='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp' id='jquery-core-js'></script>
 <script id='zxcvbn-async-js-extra'>
 var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
 </script>
-<script src='/https://creativesy.github.io/bogdan.pro/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
-<script src='/https://creativesy.github.io/bogdan.pro/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
+<script src='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
+<script src='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
 <script id='wp-polyfill-js-after'>
 ( 'fetch' in window ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"></scr' + 'ipt>' );( document.contains ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"></scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"></scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"></scr' + 'ipt>' );
 </script>
-<script src='/https://creativesy.github.io/bogdan.pro/wp-includes/js/dist/i18n.min.js?ver=bb7c3c45d012206bfcd73d6a31f84d9e' id='wp-i18n-js'></script>
+<script src='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-includes/js/dist/i18n.min.js?ver=bb7c3c45d012206bfcd73d6a31f84d9e' id='wp-i18n-js'></script>
 <script id='password-strength-meter-js-extra'>
 var pwsL10n = {"unknown":"Putere parol\u0103 necunoscut\u0103","short":"Foarte slab\u0103","bad":"Slab\u0103","good":"Medie","strong":"Puternic\u0103","mismatch":"Nepotrivire"};
 </script>
@@ -73,12 +73,12 @@ var pwsL10n = {"unknown":"Putere parol\u0103 necunoscut\u0103","short":"Foarte s
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2020-09-03 13:43:29+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=3; plural=(n == 1) ? 0 : ((n == 0 || n % 100 >= 2 && n % 100 <= 19) ? 1 : 2);","lang":"ro"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s este \u00eenvechit\u0103 \u00eencep\u00e2nd cu versiunea %2$s! Folose\u0219te %3$s \u00een locul ei. Te rog ia \u00een considerare scrierea unui cod mai comprehensiv."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
-<script src='/https://creativesy.github.io/bogdan.pro/wp-admin/js/password-strength-meter.min.js?ver=5.5.1' id='password-strength-meter-js'></script>
-<script src='/https://creativesy.github.io/bogdan.pro/wp-includes/js/underscore.min.js?ver=1.8.3' id='underscore-js'></script>
+<script src='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-admin/js/password-strength-meter.min.js?ver=5.5.1' id='password-strength-meter-js'></script>
+<script src='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-includes/js/underscore.min.js?ver=1.8.3' id='underscore-js'></script>
 <script id='wp-util-js-extra'>
 var _wpUtilSettings = {"ajax":{"url":"\/bogdan.pro\/bogdan.pro\/wp-admin\/admin-ajax.php"}};
 </script>
-<script src='/https://creativesy.github.io/bogdan.pro/wp-includes/js/wp-util.min.js?ver=5.5.1' id='wp-util-js'></script>
+<script src='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-includes/js/wp-util.min.js?ver=5.5.1' id='wp-util-js'></script>
 <script id='user-profile-js-translations'>
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
@@ -86,7 +86,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/bogdan.pro\/bogdan.pro\/wp-admin\/admin-
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2020-09-03 13:43:29+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=3; plural=(n == 1) ? 0 : ((n == 0 || n % 100 >= 2 && n % 100 <= 19) ? 1 : 2);","lang":"ro"},"Your new password has not been saved.":["Noua ta parol\u0103 nu a fost salvat\u0103."],"Hide":["Ascunde"],"Show":["Afi\u0219eaz\u0103"],"Confirm use of weak password":["Confirm\u0103 folosirea unei parole slabe"],"Hide password":["Ascunde parola"],"Show password":["Arat\u0103 parola"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
-<script src='/https://creativesy.github.io/bogdan.pro/wp-admin/js/user-profile.min.js?ver=5.5.1' id='user-profile-js'></script>
+<script src='http://localhost/https:/creativesy.github.io/bogdan.pro/wp-admin/js/user-profile.min.js?ver=5.5.1' id='user-profile-js'></script>
 	<script>
 	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
 	</script>
